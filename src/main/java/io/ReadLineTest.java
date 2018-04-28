41e8c77e9562f283615ac0826e9b8a6c87fa5384
huangkangji <huangkangji@goodsogood.com>
2018-04-28 16:26:58 +0800

    新增读读取文件，以行的形式

--- src/main/java/io/ReadLineTest.java
+++ src/main/java/io/ReadLineTest.java
@@ -1,11 +1,9 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -17,26 +15,43 @@ public class ReadLineTest {
      {
 
           //BufferedReader是可以按行读取文件
-          FileInputStream inputStream = new FileInputStream("d://Test.txt");
-          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+          //FileInputStream inputStream = new FileInputStream("d://Test.txt");
+          //BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+          //
+          //String str = null;
+          //while((str = bufferedReader.readLine()) != null)
+          //{
+          //     List<String> list = new ArrayList<>();
+          //     String[] arr = str.split(" ");
+          //     Arrays.stream(arr).forEach( s -> list.add(s));
+          //     int len = list.size();
+          //     if(len != 4) {
+          //          System.out.println(">>>>>> " + list);
+          //     }
+          //     System.out.println("INSERT INTO `ows`.`t_option` (`code`, `op_key`, `op_value`) VALUES ('1012', '"+list.get(2)+"', '"+list.get(0)+"');");
+          //
+          //}
+          //
+          ////close
+          //inputStream.close();
+          //bufferedReader.close();
 
-          String str = null;
-          while((str = bufferedReader.readLine()) != null)
-          {
-               List<String> list = new ArrayList<>();
-               String[] arr = str.split(" ");
-               Arrays.stream(arr).forEach( s -> list.add(s));
-               int len = list.size();
-               if(len != 4) {
-                    System.out.println(">>>>>> " + list);
-               }
-               System.out.println("INSERT INTO `ows`.`t_option` (`code`, `op_key`, `op_value`) VALUES ('1012', '"+list.get(2)+"', '"+list.get(0)+"');");
 
+          //ReadLineJDK18();
+          ReadLineJDK17();
+
+     }
+
+     public static void ReadLineJDK18() throws IOException {
+          //流的形式
+          Files.lines(Paths.get("D:\\Test.txt"), StandardCharsets.UTF_8).forEach(System.out::println);
+     }
+
+     public static void ReadLineJDK17() throws IOException {
+          //jdk1.7
+          List<String> lines = Files.readAllLines(Paths.get("D:\\Test.txt"), StandardCharsets.UTF_8);
+          for(String line : lines){
+               System.out.println(line);
           }
-
-          //close
-          inputStream.close();
-          bufferedReader.close();
-
      }
 }
