16a221917dc39020f598ea57ecc632b80143874a
huangkangji <huangkangji@goodsogood.com>
2018-12-24 15:34:28 +0800

    反射同一个方法名，不同的参数

--- src/main/java/reflect/clazz/Clazz.java
+++ src/main/java/reflect/clazz/Clazz.java
@@ -21,6 +21,7 @@ public class Clazz {
 		try {
 			clazz = (Class<Clazz>) Class.forName("reflect.clazz.Clazz");
 			try {
+				//测试参数为map
 				Method m = clazz.getDeclaredMethod("print", Map.class);
 				Map<String, Object> map = new HashMap<String, Object>();
 				map.put("test", "test");
@@ -33,9 +34,16 @@ public class Clazz {
 				} catch (InvocationTargetException e) {
 					e.printStackTrace();
 				}
+
+				//测试参数为string
+				Method m2 = clazz.getDeclaredMethod("print", String.class);
+				m2.invoke(clazz, "this is a test");
+
 			} catch (NoSuchMethodException e) {
 				e.printStackTrace();
 			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (Exception e ){
 				e.printStackTrace();
 			}
 		} catch (ClassNotFoundException e) {
@@ -44,6 +52,10 @@ public class Clazz {
 	}
 	
 	public static void print(Map<String, Object> map){
-		System.out.println("反射实现一个类" + map);
+		System.out.println("反射实现一个类 map = " + map);
+	}
+
+	public static void print(String test){
+		System.out.println("反射实现一个类 test = " + test);
 	}
 }
